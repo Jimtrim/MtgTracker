3570ebbdc8d083fab109297d3b8416d783939dac
Jim Frode Hoff <jimfrodehoff@gmail.com>
2016-10-18 03:17:25 +0200

    create migration for Player

---
+++ database/migrations/2016_10_18_011532_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    	Schema::create('players', function(Blueprint $table) {
+			$table->string('firstName');
+			$table->string('lastName');
+			$table->string('DCI');
+			$table->string('tournaments');
+		    $table->timestamps();
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
